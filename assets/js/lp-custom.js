49fd2669067399e89c5807c021844ab560dc894a
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-10-02 20:27:41 +0700

    update file

--- assets/js/lp-custom.js
+++ assets/js/lp-custom.js
@@ -13,6 +13,20 @@ jQuery(function($){
         cssEase: 'linear',
     });
 
+    $('.card-list').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
+        arrows: false,
+        dots: false,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        variableWidth:true,
+        centerMode: true,
+        cssEase: 'linear',
+    });
+
     $('.faq-title').on("click", function () {
         var $content = $(this).closest(".faq-item").find(".faq-content");
         var $faqplus = $(this).closest(".faq-item").find(".faq-plus");
@@ -27,60 +41,5 @@ jQuery(function($){
         $faqplus.removeClass("faq-add");
         $faqplus.addClass("faq-remove");
         }
-    });
-
-    $(window).scroll(function() {
-    
-        var scroll_all_H = $(".card-list").innerHeight() * 3.31;
-        var scroll_start = $(".card").offset().top;
-        var scroll_center = $(window).height() / 2;
-        $(".card").css({"height":scroll_all_H});
-    
-    
-        var dami_space = $(".card-list").innerHeight() / 2;
-    
-    
-        var scroll_center2 = scroll_center + 15;
-        $("#slide_contena_up").css({"height":scroll_center2});
-        $("#slide_contena_down").css({"height":scroll_center2});
-    
-        var action_side = Math.min($(window).scrollTop() - scroll_start + scroll_center, scroll_all_H);
-        if(action_side < 0){
-            action_side = 0;
-        }
-    
-    
-        var active_no = (action_side / scroll_all_H) * 3.0 + 0;
-    
-        if(active_no <= 0.8){
-            $(".card-list").removeClass("active02");
-            $(".card-list").removeClass("active03");
-            $(".card-list").addClass("active01");
-        }else if(active_no > 0.8 && active_no <= 2){
-            $(".card-list").removeClass("active01");
-            $(".card-list").removeClass("active03");
-            $(".card-list").addClass("active02");
-        }else if(active_no > 2){
-            $(".card-list").removeClass("active01");
-            $(".card-list").removeClass("active02");
-            $(".card-list").addClass("active03");
-        }
-    
-    
-        ///////////////////////////////////////////////////////////////////
-    
-    
-        var scrollPosition = $(window).scrollTop();
-        var windH = $(window).height();
-    
-    
-        $(".test").each(function(i) {
-            var thisH = $(this).offset().top;
-            if(thisH-windH*0.15 < scrollPosition){
-                $(".test").addClass("active");
-            }else{
-                $(".test").removeClass("active");
-            }
-        });
-    });    
+    });   
 });
